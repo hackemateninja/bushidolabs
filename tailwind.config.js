074a9db2f9e33249c770d2253567fa499eb708c6
Herman <nitrowitty@gmail.com>
2021-01-23 14:46:24 -0600

    add changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/components/**/*.ts', './src/pages/**/*.ts', './src/layouts/**/*.ts'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
